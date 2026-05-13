f181e5085ccb269de828a475e4a831cef7a0f64d
ariasmacc <ariacaden10@gmail.com>
2026-05-13 16:00:51 +0800

    middleware edit

--- server/server.js
+++ server/server.js
@@ -132,16 +132,16 @@ app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 
 // --- Middleware ---
-const corsOptions = {
-  origin: true,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-};
-app.use(cors(corsOptions));
-
-// Handle preflight requests
-app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 
 app.use(express.json());
