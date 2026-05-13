95c5a262ee20974ec0ace5db8acf2c91a640812d
ariasmacc <ariacaden10@gmail.com>
2026-05-13 16:27:58 +0800

    Fix CORS credentials with specific origin

--- server/server.js
+++ server/server.js
@@ -133,7 +133,7 @@ app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 // --- Middleware ---
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL || 'http://localhost:5173');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
